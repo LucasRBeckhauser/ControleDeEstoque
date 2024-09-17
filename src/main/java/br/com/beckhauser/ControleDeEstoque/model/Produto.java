6647fad805e2d559bb67a3f0c3a7ca8095c976f3
lucas.beckhauser <lucasbeckh@live.com>
2024-09-17 20:09:09 -0300

    Constructor, Getter e Setter da classe Produto.

--- src/main/java/br/com/beckhauser/ControleDeEstoque/model/Produto.java
+++ src/main/java/br/com/beckhauser/ControleDeEstoque/model/Produto.java
@@ -18,4 +18,60 @@ public class Produto extends EntityId {
     @Column(name= "quantidade_maxima",nullable = false)
     private Integer quantidadeMaxima;
 
+    @Column(name= "quantidade_minima",nullable = false)
+    private Integer quantidadeMinima;
+
+
+    public Produto() {
+    }
+
+    public Produto(String nome, String descricao, Double preco, Integer quantidadeMaxima, Integer quantidadeMinima) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidadeMaxima = quantidadeMaxima;
+        this.quantidadeMinima = quantidadeMinima;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQuantidadeMaxima() {
+        return quantidadeMaxima;
+    }
+
+    public void setQuantidadeMaxima(Integer quantidadeMaxima) {
+        this.quantidadeMaxima = quantidadeMaxima;
+    }
+
+    public Integer getQuantidadeMinima() {
+        return quantidadeMinima;
+    }
+
+    public void setQuantidadeMinima(Integer quantidadeMinima) {
+        this.quantidadeMinima = quantidadeMinima;
+    }
+
+
 }
