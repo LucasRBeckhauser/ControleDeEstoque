1985c7fd9a8e93af1fb0eb105765ca3c749cb4e8
lucasroquebeckhauser <lucasbeckh@live.com>
2024-09-24 12:49:00 -0300

    POST -> CompraService Exceção personalizada implementada

---
+++ src/main/java/br/com/beckhauser/ControleDeEstoque/service/CompraService.java
@@ -0,0 +1,42 @@
+package br.com.beckhauser.ControleDeEstoque.service;
+
+import br.com.beckhauser.ControleDeEstoque.model.Compra;
+import br.com.beckhauser.ControleDeEstoque.model.CompraItem;
+import br.com.beckhauser.ControleDeEstoque.model.Produto;
+import br.com.beckhauser.ControleDeEstoque.repository.CompraRepository;
+import br.com.beckhauser.ControleDeEstoque.repository.ProdutoRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CompraService {
+
+    private final CompraRepository compraRepository;
+    private final ProdutoRepository produtoRepository;
+
+    public CompraService(CompraRepository compraRepository, ProdutoRepository produtoRepository) {
+        this.compraRepository = compraRepository;
+        this.produtoRepository = produtoRepository;
+    }
+
+    @Transactional
+    public Compra realizarCompra(Compra compra) throws Exception {
+
+        // Validação de Compra itens | POST
+        for (CompraItem item : compra.getItensCompra()) {
+            Produto produto = produtoRepository.findById(item.getProduto().getId())
+                    .orElseThrow(() -> new Exception("Produto não encontrado: " + item.getProduto().getNome()));
+
+            if (item.getQuantidadeComprada() <= 0) {
+                throw new Exception("Quantidade comprada deve ser positiva para o produto: " + produto.getNome());
+            }
+
+            // Atualiza o estoque do produto
+            produto.setQuantidadeEmEstoque(produto.getQuantidadeEmEstoque() + item.getQuantidadeComprada());
+            produtoRepository.save(produto);
+        }
+
+        return compraRepository.save(compra);
+    }
+
+}
