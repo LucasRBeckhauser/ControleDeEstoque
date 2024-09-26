065c79151df4f9f14b5ddb3f15cec8a2cbc39645
lucasroquebeckhauser <lucasbeckh@live.com>
2024-09-25 21:19:20 -0300

    classe usuário

---
+++ src/main/java/br/com/beckhauser/ControleDeEstoque/model/Usuario.java
@@ -0,0 +1,16 @@
+package br.com.beckhauser.ControleDeEstoque.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_users")
+public class Usuario {
+
+    @Column(name = "nome_usuario", nullable = false)
+    private String usuario;
+
+    @Column(name = "senha_usuario", nullable = false)
+    private String senha;
+
+
+}
