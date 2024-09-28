053e72a1dd6b15910f85cf93b37024defd28461e
lucasroquebeckhauser <lucasbeckh@live.com>
2024-09-27 21:14:45 -0300

    FInalização EntradaController

--- src/main/java/br/com/beckhauser/ControleDeEstoque/controller/EntradaController.java
+++ src/main/java/br/com/beckhauser/ControleDeEstoque/controller/EntradaController.java
@@ -4,41 +4,40 @@ import br.com.beckhauser.ControleDeEstoque.model.Entrada;
 import br.com.beckhauser.ControleDeEstoque.service.EntradaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
 
+@RestController
+@RequestMapping("/entrada")
 public class EntradaController {
 
     @Autowired
     private EntradaService service;
 
     @PostMapping
-    public ResponseEntity create(@RequestBody Entrada entity) {
+    public ResponseEntity realizarCompra(@RequestBody Entrada entity) {
         Entrada save = service.realizarCompra(entity);
         return ResponseEntity.created(URI.create("/entrada" + entity.getId())).body(save);
     }
 
     @GetMapping
-    public ResponseEntity findAll(){
+    public ResponseEntity acharTodos() {
         List<Entrada> doacao = service.listarCompras();
         return ResponseEntity.ok(doacao);
     }
 
-
-//    @PutMapping("{id}")
-//    public  ResponseEntity update(@PathVariable("id") Long id, @RequestBody Doacao entity) {
-//        Doacao alterado = service.alterar(id, entity);
-//        return ResponseEntity.ok().body(alterado);
-//    }
-//
-//    @DeleteMapping("{id}")
-//    public ResponseEntity remove(@PathVariable("id") Long id) {
-//        service.remover(id);
-//        return ResponseEntity.noContent().build();
+    @PutMapping("{id}")
+    public ResponseEntity alterarEntrada(@PathVariable("id") Long id, @RequestBody Entrada entity) {
+        Entrada alterado = service.alterarEntrada(id, entity);
+        return ResponseEntity.ok().body(alterado);
     }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity removeEntrada(@PathVariable("id") Long id) {
+        service.excluirEntrada(id);
+        return ResponseEntity.noContent().build();
+    }
+}
 
---
+++ src/main/java/br/com/beckhauser/ControleDeEstoque/controller/ProdutoController.java
@@ -0,0 +1,4 @@
+package br.com.beckhauser.ControleDeEstoque.controller;
+
+public class ProdutoController {
+}
