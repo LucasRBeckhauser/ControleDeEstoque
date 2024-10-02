3c37ead0c3308fa38b3f87c9ac0844b46f18ab5c
lucasroquebeckhauser <lucasbeckh@live.com>
2024-10-01 22:54:03 -0300

    IMplementação classes spring security

--- src/main/java/br/com/beckhauser/ControleDeEstoque/service/AutorizationService.java
+++ src/main/java/br/com/beckhauser/ControleDeEstoque/service/AutorizationService.java
@@ -1,5 +1,7 @@
 package br.com.beckhauser.ControleDeEstoque.service;
 
+import br.com.beckhauser.ControleDeEstoque.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +9,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AutorizationService implements UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return repository.findByLogin(username);
     }
 }
